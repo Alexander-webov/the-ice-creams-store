d94444d7326139b2d043e7d148117e4ce7972cb7
Alexander-webov <vilmenshtain@yandex.ru>
2026-02-05 15:39:51 -0500

    added page cart

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,7 +1,7 @@
 import TitleSection from "../components/ui/TitleSection";
 import Container from "../components/Container";
 import Posts from "../components/blog-posts/Posts";
-
+export const dynamic = "force-dynamic";
 function Blog() {
   return (
     <main>
